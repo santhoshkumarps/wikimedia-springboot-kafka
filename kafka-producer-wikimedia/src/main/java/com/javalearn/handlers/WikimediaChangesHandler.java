5fc5887cb204c61926f1b818bbf9b1a6e2981e07
santhoshkumarps <san14kumarps@gmail.com>
2024-07-05 18:11:17 +0530

    updated commit

--- kafka-producer-wikimedia/src/main/java/com/javalearn/handlers/WikimediaChangesHandler.java
+++ kafka-producer-wikimedia/src/main/java/com/javalearn/handlers/WikimediaChangesHandler.java
@@ -4,7 +4,6 @@ import com.launchdarkly.eventsource.MessageEvent;
 import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
 
